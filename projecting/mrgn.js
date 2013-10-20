10d57b36460ed7ef89745af0bd4174c5bb8b9015
Ulrich Stroetz <ustroetz@gmail.com>
2013-10-19 20:26:17 -0700

    worki

--- projecting/mrgn.js
+++ projecting/mrgn.js
@@ -1,39 +1,40 @@
-var style = {
-    fillColor: '#000',
-    fillOpacity: 0.1,
-    strokeWidth: 0
-};
-
-var map = new OpenLayers.Map('map');
-var layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
-var vector = new OpenLayers.Layer.Vector('vector');
-map.addLayers([layer, vector]);
-
-var geolocate = new OpenLayers.Control.Geolocate({
-    bind: false,
-    geolocationOptions: {
-        enableHighAccuracy: false,
-        maximumAge: 0,
-        timeout: 7000
-    }
+var map = new OpenLayers.Map('map', {
+    controls: [new OpenLayers.Control.Navigation()] // removes default controls
 });
-map.addControl(geolocate);
+var layerOSM = new OpenLayers.Layer.OSM( "Simple OSM Map");
+var vector = new OpenLayers.Layer.Vector('vector');
 
-var firstGeolocation = true;
+
+// find users location
+var geolocate = new OpenLayers.Control.Geolocate();
+map.addControl(geolocate);
 geolocate.events.register("locationupdated",geolocate,function(e) {
-    vector.removeAllFeatures();
     var circle = new OpenLayers.Feature.Vector(
         OpenLayers.Geometry.Polygon.createRegularPolygon(
             new OpenLayers.Geometry.Point(e.point.x, e.point.y),
             e.position.coords.accuracy,
             40,
             0
-        ),
-        {},
-        style
+        )
     );
     vector.addFeatures([circle]);
     map.zoomToExtent(vector.getDataExtent());	
-	vector.removeAllFeatures();		  
 });
 geolocate.activate();
+
+
+//Create layer that queries bike racks from overpass api
+var layerBikeRack = new OpenLayers.Layer.Vector("Point", {
+    strategies: [new OpenLayers.Strategy.Fixed()],
+    protocol: new OpenLayers.Protocol.HTTP({
+        url: "http://www.overpass-api.de/api/xapi?node[bbox=-122.9130,45.4066,-122.3946,45.6788][amenity=bicycle_parking]",
+        format: new OpenLayers.Format.OSM()
+    }),
+});
+
+// add layers to map
+map.addLayers([layerOSM]);
+map.addLayers([layerBikeRack]);
+
+// NEEDS WORK: get bounding box of user area and pass it into the url
+// console.log(OpenLayers.Feature.Geometry.getBounds);
